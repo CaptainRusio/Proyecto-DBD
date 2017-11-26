e89ea3517063360eaa021a8388f1fe1a44a73f45
CaptainRusio <enrique.aviles.m@usach.cl>
2017-11-26 19:51:25 -0300

    show blade

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,7 +6,8 @@
 	@foreach($posts1 as $post)
 		<div class = "row">
 					<div class="col-md-12" align="center">
-						<h2>{{$post->title}}</h2>
+						<h2><a href="{{route('post_path',['post' => $post->id])}}">
+							{{$post->title}}</a></h2>
 						
 						<p>Posted {{$post->created_at->diffForHumans() }} </p>		
 
