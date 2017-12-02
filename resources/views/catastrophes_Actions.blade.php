8979d31421141bd0718cfd7a54f5f020cd423f58
michaelCastillo <michael.castillo.p96@gmail.com>
2017-12-01 22:46:47 -0300

    arreglada la vista

--- resources/views/catastrophes_Actions.blade.php
+++ resources/views/catastrophes_Actions.blade.php
@@ -366,9 +366,10 @@ catastrofes
 					      						<form method="post" action="publishTwitter" > 
 								                    <input type = "hidden" name = "_token" value="{{ csrf_token()}}"> 
 								                    <input type="hidden" name="id" value="{{$catastrophes[$i]->id}}"> 
-								                    @if($catastrophe->confirmed)
+								                    
 								                    <button type = "submit" class = "col-md-2 btn btn-info" 
-								                        id = "{{$catastrophes[$i]->id}}" >Habilitar </button> 
+								                        id = "{{$catastrophes[$i]->id}}" >Habilitar </button>
+
 								                        <!-- En esta opción se debe hacer el método post para habilitar una catastrofe y escribirla en Twitter-->
 								                     <input type = "hidden" name = "_token" value="{{ csrf_token()}}"> 
 								                    <input type="hidden" name="id" value="{{$catastrophes[$i]->id}}"> 
