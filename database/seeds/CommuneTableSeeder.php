f6147612c54353098638d8874949d772a75795a8
michaelCastillo <michael.castillo.p96@gmail.com>
2017-12-01 22:51:36 -0300

    con comunas con provincias id = 1

--- database/seeds/CommuneTableSeeder.php
+++ database/seeds/CommuneTableSeeder.php
@@ -10,7 +10,7 @@ class CommuneTableSeeder extends Seeder
     public function run()
     {
         $arrayCommunes = ['Arica','Camarones','Putre','General Lagos','Iquique','Alto Hospicio','Pozo Almonte','Camiña','Colchane','Huara','Pica'];
-        $arrayProvinces = [2,2,3,3,4,4,5,5,5,5,5];
+        $arrayProvinces = [1,1,2,2,3,3,4,4,5,5,5,5,5];
         for ($i=0; $i < count($arrayCommunes); $i++) { 
              DB::table('communes')->insert([
             'name'=> $arrayCommunes[$i],
